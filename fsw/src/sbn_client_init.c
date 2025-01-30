091da6d7c189b43acd1759df85c96298ec4ac8ba
rjbrown6 <Robert.J.Brown@nasa.gov>
2025-01-30 15:23:54 -0500

    [nasa/nos3#483] Update Deprecated gethostbyname to getaddrinfo

--- fsw/src/sbn_client_init.c
+++ fsw/src/sbn_client_init.c
@@ -17,10 +17,13 @@
 #include "sbn_client_utils.h"
 
 /* Start additional includes for hostname snippet */
-#include<sys/socket.h>
-#include<netdb.h>	//hostent
-#include<arpa/inet.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 /* End additional includes for hostname snippet */
 
 
@@ -31,7 +34,6 @@ extern int sbn_client_cpuId;
 pthread_t receive_thread_id;
 pthread_t heart_thread_id;
 
-
 int32 SBN_Client_Init(void)
 {
     int32 status = SBN_CLIENT_NO_STATUS_SET;
@@ -40,41 +42,39 @@ int32 SBN_Client_Init(void)
 
     log_message("SBN_Client Resolving Name %s\n", SBN_CLIENT_IP_ADDR);
 
-    /* 
-        DNS Resolution for FSW Container 
-        Start hostname snippet from: https://stackoverflow.com/questions/38002016/problems-with-gethostbyname-c
-    */
-    struct hostent *he;
-    struct in_addr **addr_list;
-    int i;
+    struct addrinfo hints, *res, *p;
+    char Addr[INET_ADDRSTRLEN] = "0.0.0.0"; // Default fallback address
+    void *addr;
+    
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;  // Use AF_UNSPEC for IPv6 support if needed
+    hints.ai_socktype = SOCK_STREAM;
 
-    char Addr[OS_MAX_API_NAME];
-
-    if ( (he = gethostbyname(SBN_CLIENT_IP_ADDR) ) != NULL) 
+    if (getaddrinfo(SBN_CLIENT_IP_ADDR, NULL, &hints, &res) == 0)
     {
-        addr_list = (struct in_addr **) he->h_addr_list;
-        for(i = 0; addr_list[i] != NULL; i++) 
+        for (p = res; p != NULL; p = p->ai_next)
         {
-            //Return the first one;
-            strcpy(Addr, inet_ntoa(*addr_list[i]) );
-            break;
+            struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+            addr = &(ipv4->sin_addr);
+
+            // Convert to string and store in Addr
+            if (inet_ntop(p->ai_family, addr, Addr, sizeof(Addr)) != NULL)
+            {
+                break;
+            }
         }
+        freeaddrinfo(res);
     }
-    /* 
-        End hostname snippet from: https://stackoverflow.com/questions/38002016/problems-with-gethostbyname-c
-    */
 
     log_message("SBN_Client Resolved %s to %s\n", SBN_CLIENT_IP_ADDR, Addr);
-    
     log_message("SBN_Client Connecting to %s, %d\n", Addr, SBN_CLIENT_PORT);
-    
+
     sbn_client_sockfd = connect_to_server(Addr, SBN_CLIENT_PORT);
-    sbn_client_cpuId = 2; /* TODO: hardcoded, but should be set by cFS SBN ??*/
+    sbn_client_cpuId = 2; /* TODO: hardcoded, but should be set by cFS SBN ?? */
 
     if (sbn_client_sockfd < 0)
     {
-        log_message(
-         "SBN_CLIENT: ERROR Failed to get sbn_client_sockfd, cannot continue.");
+        log_message("SBN_CLIENT: ERROR Failed to get sbn_client_sockfd, cannot continue.");
         status = SBN_CLIENT_BAD_SOCK_FD_EID;
     }
     else
@@ -83,33 +83,26 @@ int32 SBN_Client_Init(void)
         CFE_SBN_Client_InitPipeTbl();
 
         /* heartbeat thread establishes live connection */
-        heart_thread_status = pthread_create(&heart_thread_id, NULL, 
-            SBN_Client_HeartbeatMinder, NULL);
-            
-        status = check_pthread_create_status(heart_thread_status, 
-            SBN_CLIENT_HEART_THREAD_CREATE_EID);
-        
+        heart_thread_status = pthread_create(&heart_thread_id, NULL, SBN_Client_HeartbeatMinder, NULL);
+        status = check_pthread_create_status(heart_thread_status, SBN_CLIENT_HEART_THREAD_CREATE_EID);
+
         /* receive thread monitors for messages */
         if (status == SBN_CLIENT_SUCCESS)
-        {    
+        {
             log_message("SBN Client Created Heart pthread");
-            receive_thread_status = pthread_create(&receive_thread_id, NULL, 
-            SBN_Client_ReceiveMinder, NULL);
-        
-            status = check_pthread_create_status(receive_thread_status, 
-                SBN_CLIENT_RECEIVE_THREAD_CREATE_EID);
-        }/* end if */ 
-        
-    }/* end if */ 
-    
+            receive_thread_status = pthread_create(&receive_thread_id, NULL, SBN_Client_ReceiveMinder, NULL);
+            status = check_pthread_create_status(receive_thread_status, SBN_CLIENT_RECEIVE_THREAD_CREATE_EID);
+        }
+    }
+
     if (status != SBN_CLIENT_SUCCESS)
     {
         log_message("SBN_Client_Init error %d\n", status);
     }
     else
     {
-        log_message("SBN Client Created Recieve pthread, init success!");    
-    }/* end if */ 
-    
+        log_message("SBN Client Created Receive pthread, init success!");
+    }
+
     return status;
-}/* end SBN_Client_Init */
+} /* end SBN_Client_Init */
